508c8e9934399753e0f0b961f0df5cf91b83a1e4
Eric Garside <garside@gmail.com>
2018-12-09 23:20:12 -0500

    Surveil on Flow

--- lib/flow/operation_base.rb
+++ lib/flow/operation_base.rb
@@ -5,7 +5,6 @@ require_relative "operation/execute"
 
 # Operations are service objects which are executed with a state.
 class OperationBase
-  include Technologic
   include Operation::Core
   include Operation::Execute
 end
