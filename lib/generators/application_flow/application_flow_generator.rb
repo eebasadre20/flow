f3b72fa8254de26ed3f51449dcc4e875ed23ecae
Eric Garside <garside@gmail.com>
2019-02-12 22:43:55 -0500

    ApplicationFlow Generator

--- lib/generators/application_flow/application_flow_generator.rb
+++ lib/generators/application_flow/application_flow_generator.rb
@@ -7,7 +7,7 @@ module Flow
     class ApplicationFlowGenerator < Rails::Generators::NamedBase
       source_root File.expand_path("templates", __dir__)
 
-      hook_for :test_framework
+      # hook_for :test_framework
 
       def create_application_flow_file
         template "application_flow.rb", File.join("app/flows/application_flow.rb")
