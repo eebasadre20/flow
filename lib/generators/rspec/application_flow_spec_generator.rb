3be39f061629f52ec6227856525dfded9b5ba78e
Eric Garside <garside@gmail.com>
2019-02-12 22:28:16 -0500

    ApplicationFlow Generator

--- lib/generators/rspec/application_flow_spec_generator.rb
+++ lib/generators/rspec/application_flow_spec_generator.rb
@@ -3,14 +3,12 @@
 require "flow"
 
 module Flow
-  module Generators
-    module Rspec
-      class ApplicationFlowSpecGenerator < Rails::Generators::NamedBase
-        source_root File.expand_path("templates", __dir__)
+  module Rspec
+    class ApplicationFlowSpecGenerator < Rails::Generators::NamedBase
+      source_root File.expand_path("templates", __dir__)
 
-        def create_spec_file
-          template "application_flow_spec.rb", File.join("spec/constructors/application_flow_spec.rb")
-        end
+      def create_spec_file
+        template "application_flow_spec.rb", File.join("spec/constructors/application_flow_spec.rb")
       end
     end
   end
