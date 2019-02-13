591094fbe8ca1dbd6af1b2eca2055397da836be7
Eric Garside <garside@gmail.com>
2019-02-13 00:01:14 -0500

    Named Generators

--- lib/generators/rspec/flow/flow_generator.rb
+++ lib/generators/rspec/flow/flow_generator.rb
@@ -2,11 +2,11 @@
 
 module Rspec
   module Generators
-    class ApplicationFlowGenerator < Rails::Generators::Base
+    class FlowGenerator < Rails::Generators::Base
       source_root File.expand_path("templates", __dir__)
 
       def create_spec_file
-        template "application_flow_spec.rb", File.join("spec/flows/application_flow_spec.rb")
+        template "flow_spec.rb.erb", File.join("spec/flows/", class_path, "#{file_name}_flow_spec.rb")
       end
     end
   end
