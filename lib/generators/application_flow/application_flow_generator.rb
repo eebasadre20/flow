7b0f24c05525fa808325d8eee3571d3eabafb31f
Eric Garside <garside@gmail.com>
2019-02-12 22:36:11 -0500

    ApplicationFlow Generator

--- lib/generators/application_flow/application_flow_generator.rb
+++ lib/generators/application_flow/application_flow_generator.rb
@@ -3,13 +3,15 @@
 require "flow"
 
 module Flow
-  class ApplicationFlowGenerator < Rails::Generators::NamedBase
-    source_root File.expand_path("templates", __dir__)
+  module Generators
+    class ApplicationFlowGenerator < Rails::Generators::NamedBase
+      source_root File.expand_path("templates", __dir__)
 
-    hook_for :test_framework
+      hook_for :test_framework
 
-    def create_application_flow_file
-      template "application_flow.rb", File.join("app/flows/application_flow.rb")
+      def create_application_flow_file
+        template "application_flow.rb", File.join("app/flows/application_flow.rb")
+      end
     end
   end
 end
--- lib/generators/rspec/application_flow_spec_generator.rb
+++ lib/generators/rspec/application_flow_spec_generator.rb
@@ -4,11 +4,13 @@ require "flow"
 
 module Flow
   module Rspec
-    class ApplicationFlowSpecGenerator < Rails::Generators::NamedBase
-      source_root File.expand_path("templates", __dir__)
+    module Generators
+      class ApplicationFlowSpecGenerator < Rails::Generators::NamedBase
+        source_root File.expand_path("templates", __dir__)
 
-      def create_spec_file
-        template "application_flow_spec.rb", File.join("spec/constructors/application_flow_spec.rb")
+        def create_spec_file
+          template "application_flow_spec.rb", File.join("spec/constructors/application_flow_spec.rb")
+        end
       end
     end
   end
