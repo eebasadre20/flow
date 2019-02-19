7461bc60eb1bb14d0e505af96cf4add1efe0f1b9
Eric Garside <garside@gmail.com>
2019-02-19 11:09:44 -0500

    Modularize Flow
    Code review cleanup

--- spec/flow/callbacks_spec.rb
+++ spec/flow/callbacks_spec.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 RSpec.describe Flow::Callbacks, type: :module do
-  it_behaves_like "an example class with callbacks", described_class, %[initialize]
+  it_behaves_like "an example class with callbacks", described_class, %i[initialize]
 end
