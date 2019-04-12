0b9a99cccfb120f24dde2dc9328b25ee0c13a604
Eric Garside <garside@gmail.com>
2019-04-12 00:40:58 -0400

    Spec Cleanup
    Code review cleanup

--- spec/integration/bottles_on_the_wall_state_spec.rb
+++ spec/integration/bottles_on_the_wall_state_spec.rb
@@ -3,7 +3,7 @@
 require_relative "../support/test_classes/bottles_on_the_wall_state"
 
 RSpec.describe BottlesOnTheWallState, type: :state do
-  subject { BottlesOnTheWallState.new(bottles_of: :test_fluid) }
+  subject { described_class.new(bottles_of: :test_fluid) }
 
   it { is_expected.to define_argument :bottles_of }
   it { is_expected.to define_option :number_to_take_down, 1 }
