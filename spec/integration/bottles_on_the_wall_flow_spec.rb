1477f5a322e128c0ffda8aee30b2a24f4cc300e1
Eric Garside <garside@gmail.com>
2019-04-12 00:38:34 -0400

    Spec Cleanup

--- spec/integration/bottles_on_the_wall_flow_spec.rb
+++ spec/integration/bottles_on_the_wall_flow_spec.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require_relative "../support/test_classes/count_out_current_bottles"
+require_relative "../support/test_classes/pass_bottles_around"
+require_relative "../support/test_classes/take_bottles_down"
 require_relative "../support/test_classes/bottles_on_the_wall_flow"
 
 RSpec.describe BottlesOnTheWallFlow, type: :flow do
