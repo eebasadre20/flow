1b3b6184792b04d9f6f5c2519eec869012630eca
Eric Garside <garside@gmail.com>
2019-04-14 21:10:41 -0400

    State Output
    Code review cleanup

--- spec/flow/state/output_spec.rb
+++ spec/flow/state/output_spec.rb
@@ -11,65 +11,7 @@ RSpec.describe Flow::State::Output, type: :module do
   ]
 
   describe ".output" do
-    subject(:define_output) { example_state_class.__send__(:output, output) }
-
-    let(:output) { Faker::Lorem.word.to_sym }
-
-    before do
-      allow(example_state_class).to receive(:define_default).and_call_original
-      allow(example_state_class).to receive(:define_attribute).and_call_original
-    end
-
-    describe "defines output" do
-      let(:default) { Faker::Lorem.word }
-
-      shared_examples_for "an output is defined" do
-        it "adds to _outputs" do
-          expect { define_output }.to change { example_state_class._outputs }.from([]).to([ output ])
-        end
-      end
-
-      context "when no block is given" do
-        subject(:define_output) { example_state_class.__send__(:output, output, default: default) }
-
-        it_behaves_like "an output is defined"
-
-        it "defines an static default" do
-          define_output
-          expect(example_state_class).to have_received(:define_default).with(output, static: default)
-        end
-      end
-
-      context "when a block is given" do
-        subject(:define_output) { example_state_class.__send__(:output, output, default: default, &block) }
-
-        let(:block) do
-          ->(_) { :block }
-        end
-
-        shared_examples_for "values are handed off to define_default" do
-          it "calls define_default" do
-            define_output
-            expect(example_state_class).to have_received(:define_default).with(output, static: default, &block)
-          end
-        end
-
-        context "with a static default" do
-          it_behaves_like "values are handed off to define_default"
-        end
-
-        context "without a static default" do
-          let(:default) { nil }
-
-          it_behaves_like "values are handed off to define_default"
-        end
-      end
-    end
-
-    it "defines an attribute" do
-      define_output
-      expect(example_state_class).to have_received(:define_attribute).with(output)
-    end
+    it_behaves_like "a class which defines into a class collection", :output, :_outputs
   end
 
   describe ".inherited" do
