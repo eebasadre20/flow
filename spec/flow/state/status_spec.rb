0485965bd387bb2395815b201a2573309da4a5b4
Eric Garside <garside@gmail.com>
2019-04-14 20:21:50 -0400

    State Status

---
+++ spec/flow/state/status_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+RSpec.describe Flow::State::Status, type: :module do
+  include_context "with an example state", [
+    ActiveModel::Model,
+    ActiveModel::Validations::Callbacks,
+    Flow::State::Attributes,
+    described_class,
+  ]
+
+  describe "#validated?" do
+    subject { example_state.validated? }
+
+    before { stub_const(Faker::Internet.domain_word.capitalize, example_state_class) }
+
+    it { is_expected.to be false }
+
+    context "when callbacks run" do
+      subject { -> { example_state.valid? } }
+
+      context "with errors" do
+        before do
+          example_state_class.__send__(:define_attribute, :name)
+          example_state_class.validates :name, presence: true
+        end
+
+        it { is_expected.not_to change { example_state.validated? }.from(false) }
+      end
+
+      context "without errors" do
+        it { is_expected.to change { example_state.validated? }.from(false).to(true) }
+      end
+    end
+  end
+end
