6bf13b524d332ac88efed39f2d6cbdbdc4c3e097
Eric Garside <garside@gmail.com>
2019-04-15 21:57:03 -0400

    Allow nil on arguments
    This closes #66

--- lib/flow/state/core.rb
+++ lib/flow/state/core.rb
@@ -6,7 +6,10 @@ module Flow
     module Core
       extend ActiveSupport::Concern
 
+      attr_reader :input
+
       def initialize(**input)
+        @input = input
         run_callbacks(:initialize) do
           input.each { |key, value| __send__("#{key}=".to_sym, value) }
         end
