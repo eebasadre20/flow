8e04548d5068fb892795682b23f11972ac46b8c9
Allen Rettberg <allen.rettberg@freshly.com>
2019-05-01 00:53:56 -0400

    add delegatioin trackers

--- lib/flow/operation/accessors.rb
+++ lib/flow/operation/accessors.rb
@@ -5,20 +5,52 @@ module Flow
     module Accessors
       extend ActiveSupport::Concern
 
+      included do
+        class_attribute :_state_readers, instance_writer: false, default: []
+        class_attribute :_state_writers, instance_writer: false, default: []
+        class_attribute :_state_accessors, instance_writer: false, default: []
+      end
+
       class_methods do
         protected
 
         def state_reader(name)
-          delegate name, to: :state
+          return unless _add_state_reader_tracker(name.to_sym)
+
+          delegate name.to_sym, to: :state
         end
 
         def state_writer(name)
-          delegate "#{name}=", to: :state
+          return unless _add_state_writer_tracker(name.to_sym)
+
+          delegate("#{name}=", to: :state)
         end
 
         def state_accessor(name)
           state_reader name
           state_writer name
+        end
+
+        private
+
+        def _add_state_reader_tracker(name)
+          return false if _state_readers.include?(name)
+
+          _add_state_accessor_tracker(name) if _state_writers.include?(name)
+          _state_readers << name
+        end
+
+        def _add_state_writer_tracker(name)
+          return false if _state_writers.include?(name)
+
+          _add_state_accessor_tracker(name) if _state_readers.include?(name)
+          _state_writers << name
+        end
+
+        def _add_state_accessor_tracker(name)
+          return if _state_accessors.include?(name)
+
+          _state_accessors << name
         end
       end
     end
