d0ae38de123a32e8ed9f166daffc560b60d75815
Allen Rettberg <allen.rettberg@freshly.com>
2019-05-01 11:53:24 -0400

    add specs for tracker arrays

--- spec/flow/operation/accessors_spec.rb
+++ spec/flow/operation/accessors_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Flow::Operation::Accessors, type: :module do
-  subject { test_class.new }
+  subject(:stateful_instance) { test_class.new }
 
   let(:example_class) { Class.new.include described_class }
   let(:test_class) do
@@ -21,16 +21,69 @@ RSpec.describe Flow::Operation::Accessors, type: :module do
     test_state_class.attr_accessor(state_attribute)
   end
 
+  shared_examples_for "it has exactly one tracker variable of type" do |tracker_type|
+    let(:tracker_name) { "_state_#{tracker_type}".pluralize }
+
+    it "sets a tracker variable" do
+      expect(subject.public_send(tracker_name).count(state_attribute)).to eq 1
+    end
+  end
+
+  shared_examples_for "it has no tracker variables of type" do |tracker_type, attribute|
+    let(:tracker_name) { "_state_#{tracker_type}".pluralize }
+
+    it "doesn't set a tracker variable" do
+      expect(subject.public_send(tracker_name).count(state_attribute)).to eq 0
+    end
+  end
+
   describe ".state_reader" do
-    before { test_class.__send__(:state_reader, state_attribute) }
+    subject do
+      test_class.__send__(:state_reader, state_attribute)
+      test_class.new
+    end
 
     it { is_expected.to delegate_method(state_attribute).to(:state) }
+
+    it_behaves_like "it has exactly one tracker variable of type", :reader
+    it_behaves_like "it has no tracker variables of type", :accessor
+
+    context "when a reader has already been defined" do
+      before { test_class.__send__(:state_reader, state_attribute) }
+
+      it_behaves_like "it has exactly one tracker variable of type", :reader
+      it_behaves_like "it has no tracker variables of type", :accessor
+    end
+
+    context "when a writer has already been defined" do
+      before { test_class.__send__(:state_writer, state_attribute) }
+
+      it_behaves_like "it has exactly one tracker variable of type", :reader
+      it_behaves_like "it has exactly one tracker variable of type", :accessor
+    end
   end
 
   describe ".state_writer" do
     before { test_class.__send__(:state_writer, state_attribute) }
 
     it { is_expected.to delegate_method(state_attribute_writer).to(:state).with_arguments(state_attribute_value) }
+
+    it_behaves_like "it has exactly one tracker variable of type", :writer
+    it_behaves_like "it has no tracker variables of type", :accessor
+
+    context "when a writer has already been defined" do
+      before { test_class.__send__(:state_writer, state_attribute) }
+
+      it_behaves_like "it has exactly one tracker variable of type", :writer
+      it_behaves_like "it has no tracker variables of type", :accessor
+    end
+
+    context "when a writer has already been defined" do
+      before { test_class.__send__(:state_reader, state_attribute) }
+
+      it_behaves_like "it has exactly one tracker variable of type", :writer
+      it_behaves_like "it has exactly one tracker variable of type", :accessor
+    end
   end
 
   describe ".state_accessor" do
@@ -38,5 +91,25 @@ RSpec.describe Flow::Operation::Accessors, type: :module do
 
     it { is_expected.to delegate_method(state_attribute).to(:state) }
     it { is_expected.to delegate_method(state_attribute_writer).to(:state).with_arguments(state_attribute_value) }
+
+    it_behaves_like "it has exactly one tracker variable of type", :writer
+    it_behaves_like "it has exactly one tracker variable of type", :reader
+    it_behaves_like "it has exactly one tracker variable of type", :accessor
+
+    context "when a writer has already been defined" do
+      before { test_class.__send__(:state_writer, state_attribute) }
+
+      it_behaves_like "it has exactly one tracker variable of type", :writer
+      it_behaves_like "it has exactly one tracker variable of type", :reader
+      it_behaves_like "it has exactly one tracker variable of type", :accessor
+    end
+
+    context "when a writer has already been defined" do
+      before { test_class.__send__(:state_reader, state_attribute) }
+
+      it_behaves_like "it has exactly one tracker variable of type", :writer
+      it_behaves_like "it has exactly one tracker variable of type", :reader
+      it_behaves_like "it has exactly one tracker variable of type", :accessor
+    end
   end
 end
