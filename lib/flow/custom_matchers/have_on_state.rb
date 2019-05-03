dea0a1b60d91294bb37ce6de8c6e19efb334b025
Jordan Minneti <jkminneti@gmail.com>
2019-05-03 13:15:57 -0400

    doc on the matcher

--- lib/flow/custom_matchers/have_on_state.rb
+++ lib/flow/custom_matchers/have_on_state.rb
@@ -1,5 +1,31 @@
 # frozen_string_literal: true
 
+# RSpec matcher for making assertions on the state of a Flow or Operation after it has been run.
+# 
+# class ExampleOperation
+#   def behavior
+#     state.foo = "some data"
+#   end
+# end
+# 
+# class ExampleFlow
+#   operations [ ExampleOperation ]
+# end
+#
+# RSpec.describe ExampleOperation, type: :operation do
+#   subject { operation.execute }
+# 
+#   it { is_expected.to have_on_state(foo: "some data") }
+#   it { is_expected.to have_on_state(foo: instance_of(String)) }
+# end
+# 
+# RSpec.describe ExampleFlow, type: :operation do
+#   subject { flow.trigger }
+# 
+#   it { is_expected.to have_on_state(foo: "some data") }
+#   it { is_expected.to have_on_state(foo: instance_of(String)) }
+# end
+
 module CustomMatchers
   def have_on_state(expectations)
     HaveOnState.new(expectations)
