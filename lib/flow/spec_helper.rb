9dd8507da4ad429f59ebb00d996b6c4873de97c5
Allen Rettberg <allen.rettberg@freshly.com>
2019-06-10 13:43:27 -0400

    move spec helpers to common dir

--- lib/flow/spec_helper.rb
+++ lib/flow/spec_helper.rb
@@ -2,6 +2,6 @@
 
 require "spicerack/spec_helper"
 
-require_relative "custom_matchers"
-require_relative "shoulda_matcher_helper"
-require_relative "rspec_configuration"
+require_relative "spec_helpers/custom_matchers"
+require_relative "spec_helpers/shoulda_matcher_helper"
+require_relative "spec_helpers/rspec_configuration"
