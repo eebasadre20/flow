e499125f2be73ed66c5141d193204d0270576c6e
Allen Rettberg <allen.rettberg@freshly.com>
2019-06-10 15:37:25 -0400

    operation failure helper, operational

--- lib/flow/spec_helper/shared_contexts/with_stubbed_operation_failure.rb
+++ lib/flow/spec_helper/shared_contexts/with_stubbed_operation_failure.rb
@@ -3,20 +3,27 @@
 # Stubs a given flow to always fail when triggered.
 # Requires the following let variables to be defined in the incnlusion context:
 # * failing_flow_class
-# * failure_name
+# * failure_problem
 RSpec.shared_context "with stubbed operation failure" do
   let(:failing_operation) do
-    Class.new(Flow::OperationBase).instance_exec(failure_name) do |failure_name|
-      failure failure_name
+    Class.new(Flow::OperationBase).tap do |klass|
+      klass.instance_exec(self) do |spec_context|
+        failure spec_context.failure_problem
 
-      def behavior
-        __send__("#{failure_name}_failure!")
+        define_method :behavior do
+          instance_eval("#{spec_context.failure_problem}_failure!")
+        end
       end
     end
   end
 
   before do
     stub_const("FailingOperation", failing_operation)
-    failing_flow_class._operations << failing_operation
+
+    failing_flow_class._operations.unshift failing_operation
+  end
+
+  after do
+    failing_flow_class._operations.shift
   end
 end
