c4e28694a8eda78c43ce3cf0dc54730443a0f7df
Eric Garside <garside@gmail.com>
2019-11-13 16:31:47 -0500

    Add Deprecation Warning

--- lib/flow/state_proxy.rb
+++ lib/flow/state_proxy.rb
@@ -11,7 +11,10 @@ module Flow
     def method_missing(method_name, *arguments, &block)
       return super unless state.respond_to?(method_name)
 
-      ActiveSupport::Deprecation.warn("Direct state access of `#{method_name}' on #{state.inspect}")
+      ActiveSupport::Deprecation.warn(
+        "Direct state access of `#{method_name}' on #{state.inspect} will be removed in a future version of flow. "\
+        "Use a state accessor instead - for more information see github/freshly/flow/deprecation_notice"
+      )
       state.public_send(method_name, *arguments, &block)
     end
 
