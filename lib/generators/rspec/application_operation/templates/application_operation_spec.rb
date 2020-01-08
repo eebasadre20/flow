7db20d3f447ffd1e43e6a406f75560c9f3fffc9d
Eric Garside <garside@gmail.com>
2020-01-08 13:28:24 -0500

    Update application_operation_spec.rb

--- lib/generators/rspec/application_operation/templates/application_operation_spec.rb
+++ lib/generators/rspec/application_operation/templates/application_operation_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe ApplicationOperation, type: :operation do
   subject { described_class }
 
   it { is_expected.to inherit_from Flow::OperationBase }
-  
+
   describe "#execute" do
     subject(:execute) { operation.execute }
     
