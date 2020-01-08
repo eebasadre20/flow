1e00a6b6c062788645a2553e2598c5ad3aed0264
Eric Garside <garside@gmail.com>
2020-01-08 12:58:58 -0500

    Update application_operation_spec.rb

--- lib/generators/rspec/application_operation/templates/application_operation_spec.rb
+++ lib/generators/rspec/application_operation/templates/application_operation_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe ApplicationOperation, type: :operation do
   subject { described_class }
 
   it { is_expected.to inherit_from Flow::OperationBase }
+  
+  describe "#execute" do
+    subject(:execute) { operation.execute }
+
+    pending "describe the effects of a successful `Operation#execute` (or delete) #{__FILE__}"
+  end
 end
